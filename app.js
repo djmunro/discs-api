5e9fbadcf3a00482c8427fdb0bdcd40cf1bc0723
David Munro <munrodj86@gmail.com>
2023-04-22 20:15:16 -0400

    updted to support vercel

--- app.js
+++ app.js
@@ -11,36 +11,44 @@ async function prismaPlugin(fastify, options) {
   })
 }
 
-const app = Fastify()
+const createFastifyApp = () => {
+  const app = Fastify()
 
-app.register(fastifyPlugin(prismaPlugin))
+  app.register(fastifyPlugin(prismaPlugin))
 
-app.get('/discs', async (request, reply) => {
-  const discs = await app.prisma.disc.findMany()
-  reply.send(discs)
-})
-
-app.post('/discs', async (request, reply) => {
-  const newDisc = await app.prisma.disc.create({
-    data: {
-      name: request.body.name,
-    },
+  app.get('/api/discs', async (request, reply) => {
+    const discs = await app.prisma.disc.findMany()
+    reply.send(discs)
   })
-  reply.send(newDisc)
-})
 
-const start = async () => {
-  try {
-    await app.listen(3000)
-    console.log('Server listening on http://localhost:3000')
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
-  }
+  app.post('/api/discs', async (request, reply) => {
+    const newDisc = await app.prisma.disc.create({
+      data: {
+        name: request.body.name,
+      },
+    })
+    reply.send(newDisc)
+  })
+
+  return app
 }
 
-if (require.main === module) {
-  start()
+const fastifyApp = createFastifyApp()
+
+const isVercel = process.env.VERCEL_REGION !== undefined
+if (isVercel) {
+  module.exports = (req, res) => {
+    fastifyApp.ready((err) => {
+      if (err) throw err
+      fastifyApp.server.emit('request', req, res)
+    })
+  }
 } else {
-  module.exports = app
+  fastifyApp.listen({ port: 3000 }, (err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log('Server listening on http://localhost:3000')
+  })
 }
